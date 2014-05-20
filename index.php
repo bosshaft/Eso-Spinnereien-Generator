7c678cbbfa40bb07370124d13ca101631c368af7
bosshaft <bosshaft@grr.la>
2014-05-20 22:19:35 +0200

    Test0r
    M3g4

--- index.php
+++ index.php
@@ -4,22 +4,6 @@
     'Unsere Idee ist, ',
     'Wir planen, ',
     'Der Erzengel Metatron hat uns auf die Idee gebracht, ',
-    'Geleitet von jahrtausendealtem Erfahrungswissen kamen wir darauf, ',
-    'Der Geist von Wilhelm Reich hat uns aufgetragen, ',
-    'Weil wir die moderne Schulmedizin ablehnen, sehen wir es als unsere Pflicht, ',
-    'Nach jahrelanger Meditation haben uns die Lichtwesen mit der Idee kontaktiert, ',
-    'Weil wir dem Chemtrail-Terror entgegentreten wollen, planen wir, ',
-    'Aufgrund unserer tiefen spirituellen Liebe zu unseren Mitmenschen kamen wir auf den Gedanken, ',
-    'Unser Templerbruder vom alten Orden hat uns geraten, ',
-    'Rudolf Steiners 4. Reinkarnation hat uns beauftragt, ',
-    'Unsere Investoren vom Planeten Nibiru zwingen uns, ',
-    'Weil wir uns gegen die Infrarot-Nazis zur Wehr setzen möchten, können wir nicht anders, als ',
-    'Von den Erfolgen der Lichtheiler ausgehend ist es uns möglich, ',
-    'Unsere Energiegruppe hat beschlossen, ',
-    'Der Terror der Merkel-Junta lässt uns keine andere Wahl, als ',
-    'Viktor Schaubergers Schriften haben uns dazu inspiriert, ',
-    'Weil wir uns dazu berufen fühlen, in die Fußstapfen Nikola Teslas zu treten, haben wir vor, ',
-    'Weil das jüdisch kontrollierte Finanzsystem uns keine andere Wahl lässt, '
   );
 
   // Warum haben sie Superkräfte?
@@ -27,19 +11,6 @@
     'hoch energetisierte ',
     'tachyonisierte ',
     'deoxidisierte',
-    'mit Plutonium abgefüllte ',
-    'homöopathisch potenzierte ',
-    'mit Bachblütenessenzen angereicherte ',
-    'mit Orgonenergie bestrahlte ',
-    'nach der Silva-Methode behandelte ',
-    'nach Plänen von Dr. Axel Stoll konstruierte ',
-    'auf vedischer Wissenschaft basierende ',
-    'mit Hilfe der Alpha-Synapsen-Programmierung korrigierte ',
-    'pseudosymmetrische',
-    'biologisch-dynamisch erzeugte ',
-    'radiästhetisch vibrierende ',
-    'raum-zeitlich nach innen gerichtete ',
-    'negativ bezinste '
   );
 
   // Und was?
@@ -47,40 +18,6 @@
     'Kopfhörer ',
     'Müsliriegel ',
     'Dildos ',
-    'Klangschalen',
-    'Kupferleitungen',
-    'Halbedelsteine ',
-    'Globuli ',
-    'Energiesparlampen ',
-    'Energiekarten ',
-    'Erfrischungsgetränke ',
-    'Bürolampen ',
-    'Akupunkturnadeln ',
-    'Voodoopuppen ',
-    'Abfalleimer ',
-    'Fledermausköttel',
-    'Toilettenpapierrollen ',
-    'USB-Kabel ',
-    'Kaffeemaschinen ',
-    'Nickel-Cadmium-Batterien ',
-    'Tennissocken ',
-    'Unterhemden ',
-    'Autoradios ',
-    'Aschenbecher ',
-    'Topflappen ',
-    'Rheumadecken ',
-    'Küchenmesser ',
-    'Parkscheiben ',
-    'unidirektionale Kabel',
-    'goldbeschichtete Glasfaserenden',
-    'Zimmerpflanzen ',
-    'Damenfahrräder ',
-    'Zahnimplantate ',
-    'Bilderrahmen ',
-    'Kopfkissen ',
-    'Käsebrötchen ',
-    'Klaviere ',
-    'Postwurfsendungen '
   );
 
   // Was tun wir?
@@ -88,20 +25,6 @@
     'mit kosmischen Strahlungen zu bombardieren ',
     'mit Mikrowellen und/oder Skalarwellen aufzuladen ',
     'durch weiße Magie zum schwingen zu bringen ',
-    'in superionisiertes Wasser einzulegen ',
-    'durch alchemistische Rituale zu transformieren ',
-    'mit Urfrequenzen zu beschallen ',
-    'gemäß eines Implosionsstrudels anzuzapfen ',
-    'mit heiligem Radium 88 zu versetzen ',
-    'mit informiertem Wasser zu beträufeln ',
-    'von Indigo-Kindern bemalen zu lassen ',
-    'pentatonisch zu beschallen ',
-    'aurafotografisch zu erfassen ',
-    'pulsierenden Magnetfeldern auszusetzen ',
-    'mit Energieakkumulatoren zu modulieren ',
-    'harmonisch auszupendeln ',
-    'neurolinguistisch zu programmieren ',
-    'homöopathisch zu verschütteln '
   );
 
   // Und warum?
@@ -109,23 +32,6 @@
     'um den Benzinverbrauch von Kraftfahrzeugen zu reduzieren.',
     'um das Dirk-Hamer-Syndrom sogenannter Krebspatienten zu behandeln.',
     'um die Mind Control-Versuche der Illuminaten abzuwehren.',
-    'um unseren Kunden zu mehr Energie und Lebensfreude zu verhelfen.',
-    'um den Kontakt zu verstorbenen Angehörigen zu ermöglichen.',
-    'um der Raffgier der Pharmaindustrie eine Alternative entgegen zu setzen.',
-    'um den Menschen den Kontakt zu ihren früheren Inkarnationen zu erlauben.',
-    'um Leidenden Teufel und Dämonen auszutreiben.',
-    'um Impfungen gegen Tropenkrankheiten unnötig zu machen.',
-    'um Big Pharma einen Strich durch die Rechnung zu machen.',
-    'um die Bevölkerung zu einem gesünderen Leben zu führen.',
-    'um den semitisch-reptiloiden Besatzern entgegen zu treten.',
-    'um auch Laien Astralreisen zu ermöglichen.',
-    'um den ungünstigen Tachyonenfluss in Altbauwohnungen zu korrigieren.',
-    'um die Aura des Anwenders positiv zu beeinflussen.',
-    'um die Wehen gebärender Frauen zu lindern.',
-    'um unfruchtbaren Frauen den Kinderwunsch zu erfüllen.',
-    'um die Selbstreinigungs- und Entschlackungsfähigkeit des Körpers anzuregen.',
-    'um den Bilderbergern auf die Schliche zu kommen.',
-    'um der Zinsknechtschaft ein Ende zu setzen.'
   );
 ?>
 <!DOCTYPE html>
